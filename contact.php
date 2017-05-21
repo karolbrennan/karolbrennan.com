f8c19f595a542dd0c5de83348e29335127ac9ada
codemasterkarol <codemasterkarol@gmail.com>
2017-05-20 22:38:14 -0500

    updating my twitter username

--- contact.php
+++ contact.php
@@ -17,7 +17,7 @@
                 <ul class="social">
                     <li><a href="http://linkedin.com/in/karolbrennan"><i class="fa fa-linkedin-square"></i> <span>LinkedIn</span></a></li>
                     <li><a href="http://github.com/karolbrennan"><i class="fa fa-github"></i> <span>GitHub</span></a></li>
-                    <li><a href="http://twitter.com/codemasterkarol"><i class="fa fa-twitter"></i> <span>Twitter</span></a></li>
+                    <li><a href="http://twitter.com/itskarolbrennan"><i class="fa fa-twitter"></i> <span>Twitter</span></a></li>
                 </ul>
             </div>
         </div>
